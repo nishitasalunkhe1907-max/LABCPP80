c5d7a936c620276f42cfc0791bf35f265ccd6ee2
nishitasalunkhe1907-max <nishitasalunkhe1907@gmail.com>
2026-07-28 04:23:44 +0000

    prime number

---
+++ primecode.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, count = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    if (n <= 1)
+    {
+        cout << n << " is not a prime number.";
+    }
+    else
+    {
+        for (i = 1; i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                count++;
+            }
+        }
+
+        if (count == 2)
+            cout << n << " is a prime number.";
+        else
+            cout << n << " is not a prime number.";
+    }
+
+    return 0;
+}
