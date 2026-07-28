785f3fcaaa90ba70ac1ecba79f3f676bf79a7a19
nishitasalunkhe1907-max <nishitasalunkhe1907@gmail.com>
2026-07-28 05:00:43 +0000

    format

---
+++ formatcode.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("(%d)\n", a);
+    printf("(%d)\n", b);
+    printf("(%d)\n", c);
+
+    return 0;
+}
