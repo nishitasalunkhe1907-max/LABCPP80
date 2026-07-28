25bed730f63de6bff3b7028be35870799fdba03a
nishitasalunkhe1907-max <nishitasalunkhe1907@gmail.com>
2026-07-28 04:41:52 +0000

    personal

---
+++ namecode.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name      : Nishita Salunkhe\n");
+    printf("Roll No.  : 80\n");
+    printf("Class     : SY CSE\n");
+    printf("Division  : C \n");
+
+    return 0;
+}
