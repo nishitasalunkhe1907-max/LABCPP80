b88714b2396fff89a80d0a4ec48008685152dadc
nishitasalunkhe1907-max <nishitasalunkhe1907@gmail.com>
2026-07-30 14:04:42 +0000

    factoial

--- factorialcode.cpp
+++ factorialcode.cpp
@@ -11,7 +11,7 @@ int main()
 
     if (n < 0)
     {
-        cout << "Factorial is not defined for negative numbers.";
+        cout << "Factorial is not defined for positive numbers.";
     }
     else
     {
